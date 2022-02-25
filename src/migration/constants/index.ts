65ae3f13116329d63078ec16a8930bcefebfdcbc
GilbertoSVN <nvotreblig@gmail.com>
2022-02-24 21:24:27 -0300

    feat: add posts migration

--- src/migration/constants/index.ts
+++ src/migration/constants/index.ts
@@ -3,3 +3,5 @@ const SCRIPTS_FOLDER = require('path').resolve(__dirname, '..', 'scripts');
 
 export const ADD_USER_UP = `${SCRIPTS_FOLDER}/addUser1645666277355.up.sql`;
 export const ADD_USER_DOWN = `${SCRIPTS_FOLDER}/addUser1645666277355.down.sql`;
+export const ADD_POSTS_UP = `${SCRIPTS_FOLDER}/addPosts1645748002913.up.sql`;
+export const ADD_POSTS_DOWN = `${SCRIPTS_FOLDER}/addPosts1645748002913.down.sql`;
