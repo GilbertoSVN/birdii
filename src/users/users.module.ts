fe03e1170f9884b54f7c5d47d6be0be547ce330a
GilbertoSVN <nvotreblig@gmail.com>
2022-02-24 21:11:41 -0300

    feat: added get one user

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,14 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CreateUserController } from './controllers/create-user.controller';
+import { GetUserController } from './controllers/get-user.controller';
 import { GetUsersController } from './controllers/get-users.controller';
 import { UsersRepository } from './repositories/users.repository';
 import { CreateUserService } from './services/create-user.service';
+import { GetUserService } from './services/get-user.service';
 import { GetUsersService } from './services/get-users.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UsersRepository])],
-  controllers: [GetUsersController, CreateUserController],
-  providers: [GetUsersService, CreateUserService],
+  controllers: [GetUsersController, CreateUserController, GetUserController],
+  providers: [GetUsersService, CreateUserService, GetUserService],
 })
 export class UsersModule {}
