e229709818a494c4fcea675e63f379992850cca8
GilbertoSVN <nvotreblig@gmail.com>
2022-02-28 20:54:45 -0300

    update: updated users to accept followers count

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,6 +5,8 @@ import { GetUserPostsController } from './controllers/get-user-posts.controller'
 import { GetUserController } from './controllers/get-user.controller';
 import { GetUsersController } from './controllers/get-users.controller';
 import { UsersRepository } from './repositories/users.repository';
+import { AddUserFollowedCountService } from './services/add-user-followed-count.service';
+import { AddUserFollowedByCountService } from './services/add-user-followedBy-count.service';
 import { AddUserPostCountService } from './services/add-user-post-count.service';
 import { CreateUserService } from './services/create-user.service';
 import { GetUserPostsService } from './services/get-user-posts.service';
@@ -25,7 +27,14 @@ import { GetUsersService } from './services/get-users.service';
     GetUserService,
     AddUserPostCountService,
     GetUserPostsService,
+    AddUserFollowedCountService,
+    AddUserFollowedByCountService,
   ],
-  exports: [GetUserService, AddUserPostCountService],
+  exports: [
+    GetUserService,
+    AddUserPostCountService,
+    AddUserFollowedCountService,
+    AddUserFollowedByCountService,
+  ],
 })
 export class UsersModule {}
